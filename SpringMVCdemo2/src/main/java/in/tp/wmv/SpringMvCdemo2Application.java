db688befa3d0bdba438c991b6d7f905d9eca9e9d
Dharini <dharinidorairaj@gmail.com>
2019-04-13 12:47:32 +0530

    sample commit

---
+++ SpringMVCdemo2/src/main/java/in/tp/wmv/SpringMvCdemo2Application.java
@@ -0,0 +1,12 @@
+package in.tp.wmv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvCdemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvCdemo2Application.class, args);
+	}
+}
---
+++ SpringMVCdemo2/src/main/java/in/tp/wmv/index.jsp
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>
+A Spring MVC crud application
+</title>
+</head>
+<body>
+<h1>
+The below are the crud links
+</h1>
+<ol>
+	<li>
+	</li>
+</ol>
+</body>
+</html>
